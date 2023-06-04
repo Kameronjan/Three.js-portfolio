3818df31a244722a5d1f7cdba4b8d224a0bb8593
Kameron <122124504+Kameronjan@users.noreply.github.com>
2023-06-04 01:28:49 -0400

    progressing

--- main.js
+++ main.js
@@ -10,8 +10,21 @@ const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
-renderer.setPixelRatio( window.devicePixelRation);
-renderer.setSize( window.innerWidth, window.innerHeight);
+renderer.setPixelRatio( window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight );
 camera.position.setZ(30);
 
-renderer.render (scene,camera);
+renderer.render( scene, camera );
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100)
+const material = new THREE.MeshBasicMaterial( { color: 0xFF6347, wireframe: true } );
+const torus = new THREE.Mesh(geometry, material );
+
+scene.add(torus)
+
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera) ;
+}
+
+animate()
